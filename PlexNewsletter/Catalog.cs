cf08f683462fbed96f9b57792db23843023794c0
Maxime Mohandi <mohandi.maxime@gmail.com>
2024-06-15 13:01:00 +0200

    refactor: 💡 move mapping from plex json to media into own func

--- PlexNewsletter/Catalog.cs
+++ PlexNewsletter/Catalog.cs
@@ -18,27 +18,34 @@ public class Catalog
     public async Task<List<Media>> Medias()
     {
         var mediaList = new List<Media>();
-        using(JsonDocument doc = JsonDocument.Parse(FetchMediaFromPlex()))
+        using (JsonDocument doc = JsonDocument.Parse(FetchMediaFromPlex()))
         {
             var root = doc.RootElement;
             var media = root.GetProperty("MediaContainer").GetProperty("Metadata");
 
             foreach (var item in media.EnumerateArray())
             {
-                var title = item.GetProperty("title").GetString();
-                var type = item.GetProperty("type").GetString();
-                var addedAt = item.GetProperty("addedAt").GetUInt32();
-                var summary = item.GetProperty("summary").GetString();
-                mediaList.Add(new Media { Title = title, type = type, AddedAt = new DateTime(1970, 1, 1, 0, 0, 0, 0, DateTimeKind.Utc).AddSeconds(addedAt), Summary = summary});
+                mediaList.Add(ConvertToMedia(item));
             }
         }
+
         return mediaList;
     }
+
     private string FetchMediaFromPlex()
     {
         var plexRecentlyAddedMediaUri = new Uri($"{PlexUrl}/library/recentlyAdded?X-Plex-Token={PlexToken}");
         var response = _client.GetAsync(plexRecentlyAddedMediaUri).Result;
         var content = response.Content.ReadAsStringAsync().Result;
         return content;
+    }
+
+    private static Media ConvertToMedia(JsonElement item)
+    {
+        var title = item.GetProperty("title").GetString();
+        var type = item.GetProperty("type").GetString();
+        var addedAt = item.GetProperty("addedAt").GetUInt32();
+        var summary = item.GetProperty("summary").GetString();
+        return new Media { Title = title, type = type, AddedAt = new DateTime(1970, 1, 1, 0, 0, 0, 0, DateTimeKind.Utc).AddSeconds(addedAt), Summary = summary};
     }
 }
