679a611f545f1312e627eb2182c6e2c0c605fd83
Maxime Mohandi <mohandi.maxime@gmail.com>
2024-09-07 20:22:56 +0200

    feat: 🎸 write tvshow to discord message

--- MediaHubNewsletter.Tests/Newsletter/Discord/DiscordMessageWriterTests.cs
+++ MediaHubNewsletter.Tests/Newsletter/Discord/DiscordMessageWriterTests.cs
@@ -12,11 +12,12 @@ public class DiscordMessageWriterTests
     {
         var expected = new DiscordMessage(new[]
         {
-            new DiscordEmbedMessage("\ud83c\udfa5 Films :", new[]
+            new DiscordEmbedMessage("**\ud83c\udfa5 Films :**", new[]
             {
                 new DiscordEmbedField("**Inception**", "Un voleur qui s'infiltre dans les rêves de ses cibles pour voler leurs secrets les plus précieux."),
                 new DiscordEmbedField("**Interstellar**", "Un groupe d'explorateurs utilise un trou de ver récemment découvert pour dépasser les limites des voyages spatiaux humains et conquérir les vastes distances d'un voyage interstellaire.")
             }),
+            new DiscordEmbedMessage("**\ud83d\udcfa Séries :**", Enumerable.Empty<DiscordEmbedField>())
         });
 
         var result = DiscordMessageWriter.Write(new[]
@@ -42,4 +43,31 @@ public class DiscordMessageWriterTests
         result.Should().BeEquivalentTo(expected);
     }
 
+    [Test]
+    public void ShouldWriteSingleEpisodeToDiscordEmbedmessage()
+    {
+        var expected = new DiscordMessage(new[]
+        {
+            new DiscordEmbedMessage("**\ud83d\udcfa Séries :**", new[]
+            {
+                new DiscordEmbedField("**The Mandalorian** - S01E02", "A Mandalorian bounty hunter tracks a target for a well-paying client.")
+            }),
+            new DiscordEmbedMessage("**\ud83c\udfa5 Films :**", Enumerable.Empty<DiscordEmbedField>())
+        });
+
+        var result = DiscordMessageWriter.Write(new[]
+        {
+            new FakeMedia()
+            {
+                Title = "Chapter 1",
+                Season = 1,
+                Episode = 2,
+                Type = MediaType.TvShow,
+                Summary = "A Mandalorian bounty hunter tracks a target for a well-paying client.",
+                TvShow = "The Mandalorian"
+            }
+        });
+
+        result.Should().BeEquivalentTo(expected);
+    }
 }
--- MediaHubNewsletter/Newsletter/Discord/DiscordMessageWriter.cs
+++ MediaHubNewsletter/Newsletter/Discord/DiscordMessageWriter.cs
@@ -10,9 +10,19 @@ public class DiscordMessageWriter
             .Where(m => m.Type == MediaType.Movie)
             .Select(m => new DiscordEmbedField($"**{m.Title}**", m.Summary));
 
+        var tvShowFields = media
+            .Where(m => m.Type == MediaType.TvShow)
+            .Select(m => new DiscordEmbedField($"**{m.TvShow}** - S{WriteEpisodeToStandard(m.Season)}E{WriteEpisodeToStandard(m.Episode)}", m.Summary));
+
         return new DiscordMessage(new List<DiscordEmbedMessage>
         {
-            new("\ud83c\udfa5 Films :", movieFields)
+            new("**\ud83c\udfa5 Films :**", movieFields),
+            new("**\ud83d\udcfa Séries :**", tvShowFields)
         });
+    }
+
+    private static string WriteEpisodeToStandard(int episode)
+    {
+        return episode <= 9 ? $"0{episode}" : episode.ToString();
     }
 }
