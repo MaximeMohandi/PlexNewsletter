b677c804510a3c43a50cb62be6b71808551e6f1f
Maxime Mohandi <mohandi.maxime@gmail.com>
2024-09-07 22:31:05 +0200

    refactor: 💡 move method

--- MediaHubNewsletter.Tests/Newsletter/Discord/DiscordMessageWriterTests.cs
+++ MediaHubNewsletter.Tests/Newsletter/Discord/DiscordMessageWriterTests.cs
@@ -8,7 +8,7 @@ namespace MediahubNewsletter.Tests.Newsletter.Discord;
 public class DiscordMessageWriterTests
 {
     [Test]
-    public void ShouldWriteMovieIntoDiscordEmbedMessage()
+    public void ShouldWriteIntoDiscordEmbedMessage()
     {
         var expected = new DiscordMessage(new[]
         {
@@ -17,7 +17,12 @@ public class DiscordMessageWriterTests
                 new DiscordEmbedField("**Inception**", "Un voleur qui s'infiltre dans les rêves de ses cibles pour voler leurs secrets les plus précieux."),
                 new DiscordEmbedField("**Interstellar**", "Un groupe d'explorateurs utilise un trou de ver récemment découvert pour dépasser les limites des voyages spatiaux humains et conquérir les vastes distances d'un voyage interstellaire.")
             }),
-            new DiscordEmbedMessage("**\ud83d\udcfa Séries :**", Enumerable.Empty<DiscordEmbedField>())
+            new DiscordEmbedMessage("**\ud83d\udcfa Séries :**", new[]
+            {
+                new DiscordEmbedField("**Ring Fit Adventure** - S01E01", "Un voleur qui s'infiltre dans les rêves de ses cibles pour voler leurs secrets les plus précieux."),
+                new DiscordEmbedField("**The Mandalorian** - S01E02-04", "A Mandalorian bounty hunter tracks a target for a well-paying client."),
+                new DiscordEmbedField("**The Mandalorian** - S02E03", "The battered Mandalorian returns to his client for his reward."),
+            })
         });
 
         var result = DiscordMessageWriter.Write(new[]
@@ -38,25 +43,15 @@ public class DiscordMessageWriterTests
                     "Un groupe d'explorateurs utilise un trou de ver récemment découvert pour dépasser les limites des voyages spatiaux humains et conquérir les vastes distances d'un voyage interstellaire.",
 
             },
-        });
-
-        result.Should().BeEquivalentTo(expected);
-    }
-
-    [Test]
-    public void ShouldWriteSingleEpisodeToDiscordEmbedmessage()
-    {
-        var expected = new DiscordMessage(new[]
-        {
-            new DiscordEmbedMessage("**\ud83d\udcfa Séries :**", new[]
+            new FakeMedia()
             {
-                new DiscordEmbedField("**The Mandalorian** - S01E02", "A Mandalorian bounty hunter tracks a target for a well-paying client.")
-            }),
-            new DiscordEmbedMessage("**\ud83c\udfa5 Films :**", Enumerable.Empty<DiscordEmbedField>())
-        });
-
-        var result = DiscordMessageWriter.Write(new[]
-        {
+                Title = "Ring Fit Adventure",
+                Season = 1,
+                Episode = 1,
+                Type = MediaType.TvShow,
+                Summary = "Un voleur qui s'infiltre dans les rêves de ses cibles pour voler leurs secrets les plus précieux.",
+                TvShow = "Ring Fit Adventure"
+            },
             new FakeMedia()
             {
                 Title = "Chapter 1",
@@ -65,55 +60,36 @@ public class DiscordMessageWriterTests
                 Type = MediaType.TvShow,
                 Summary = "A Mandalorian bounty hunter tracks a target for a well-paying client.",
                 TvShow = "The Mandalorian"
+            },
+            new FakeMedia()
+            {
+                Title = "Chapter 2",
+                Season = 1,
+                Episode = 3,
+                Type = MediaType.TvShow,
+                Summary = "The battered Mandalorian returns to his client for his reward.",
+                TvShow = "The Mandalorian"
+            },
+            new FakeMedia()
+            {
+                Title = "Chapter 3",
+                Season = 1,
+                Episode = 4,
+                Type = MediaType.TvShow,
+                Summary = "The Mandalorian teams up with an ex-soldier to protect a village from raiders.",
+                TvShow = "The Mandalorian"
+            },
+            new FakeMedia()
+            {
+                Title = "Chapter 4",
+                Season = 2,
+                Episode = 3,
+                Type = MediaType.TvShow,
+                Summary = "The battered Mandalorian returns to his client for his reward.",
+                TvShow = "The Mandalorian"
             }
         });
 
         result.Should().BeEquivalentTo(expected);
-    }
-
-    [Test]
-    public void ShouldAggregateWhenEpisodeFollows()
-    {
-       var expected = new DiscordMessage(new[]
-       {
-           new DiscordEmbedMessage("**\ud83d\udcfa Séries :**", new[]
-           {
-               new DiscordEmbedField("**The Mandalorian** - S01E02-04", "A Mandalorian bounty hunter tracks a target for a well-paying client."),
-           }),
-           new DiscordEmbedMessage("**\ud83c\udfa5 Films :**", Enumerable.Empty<DiscordEmbedField>())
-       });
-
-       var result = DiscordMessageWriter.Write(new[]
-       {
-           new FakeMedia()
-           {
-               Title = "Chapter 1",
-               Season = 1,
-               Episode = 2,
-               Type = MediaType.TvShow,
-               Summary = "A Mandalorian bounty hunter tracks a target for a well-paying client.",
-               TvShow = "The Mandalorian"
-           },
-           new FakeMedia()
-           {
-               Title = "Chapter 2",
-               Season = 1,
-               Episode = 3,
-               Type = MediaType.TvShow,
-               Summary = "The battered Mandalorian returns to his client for his reward.",
-               TvShow = "The Mandalorian"
-           },
-           new FakeMedia()
-           {
-               Title = "Chapter 3",
-               Season = 1,
-               Episode = 4,
-               Type = MediaType.TvShow,
-               Summary = "The Mandalorian teams up with an ex-soldier to protect a village from raiders.",
-               TvShow = "The Mandalorian"
-           }
-       });
-
-       result.Should().BeEquivalentTo(expected);
     }
 }
--- MediaHubNewsletter/Newsletter/Discord/DiscordMessageWriter.cs
+++ MediaHubNewsletter/Newsletter/Discord/DiscordMessageWriter.cs
@@ -4,43 +4,43 @@ namespace MediahubNewsletter.Newsletter.Discord;
 
 public static class DiscordMessageWriter
 {
-    public static DiscordMessage Write(IEnumerable<IMedia> media)
+    public static DiscordMessage Write(IEnumerable<IMedia> medias)
     {
-        var movieFields = media
-            .Where(m => m.Type == MediaType.Movie)
-            .Select(m => new DiscordEmbedField($"**{m.Title}**", m.Summary));
-
-        var tvShowFields = media
-            .Where(m => m.Type == MediaType.TvShow);
-
-
         return new DiscordMessage(new List<DiscordEmbedMessage>
         {
-            new("**\ud83c\udfa5 Films :**", movieFields),
-            new("**\ud83d\udcfa Séries :**", AggregateFollowingEpisode(tvShowFields))
+            medias.WriteMovieToDiscord(),
+            medias.WriteTvShowToDiscord()
         });
     }
 
     private static string FormatToSxxEyy(int number) => number <= 9 ? $"0{number}" : number.ToString();
 
-    private static IEnumerable<DiscordEmbedField> AggregateFollowingEpisode(IEnumerable<IMedia> medias)
-    {
-        var groupedTvShows = medias
+    private static DiscordEmbedMessage WriteTvShowToDiscord(this IEnumerable<IMedia> medias) =>
+        new ("**\ud83d\udcfa Séries :**", medias
+            .Where(m => m.Type == MediaType.TvShow)
             .GroupBy(m => m.TvShow)
             .Select(shows => shows
                 .GroupBy(m => m.Season)
                 .Select(season =>
                 {
-                   if(season.Count() > 1)
-                   {
-                       var firstEpisode = season.First();
-                       var lastEpisode = season.Last();
-                       return new DiscordEmbedField($"**{firstEpisode.TvShow}** - S{FormatToSxxEyy(firstEpisode.Season)}E{FormatToSxxEyy(firstEpisode.Episode)}-{FormatToSxxEyy(lastEpisode.Episode)}", firstEpisode.Summary);
-                   }
-                       var episode = season.First();
-                       return new DiscordEmbedField($"**{episode.TvShow}** - S{FormatToSxxEyy(episode.Season)}E{FormatToSxxEyy(episode.Episode)}", episode.Summary);
-                }));
+                    var firstEpisode = season.First();
+                    if (season.Count() > 1)
+                    {
 
-        return groupedTvShows.SelectMany(x => x);
-    }
+                        var lastEpisode = season.Last();
+                        return new DiscordEmbedField(
+                            $"**{firstEpisode.TvShow}** - S{FormatToSxxEyy(firstEpisode.Season)}E{FormatToSxxEyy(firstEpisode.Episode)}-{FormatToSxxEyy(lastEpisode.Episode)}",
+                            firstEpisode.Summary);
+                    }
+
+                    return new DiscordEmbedField(
+                        $"**{firstEpisode.TvShow}** - S{FormatToSxxEyy(firstEpisode.Season)}E{FormatToSxxEyy(firstEpisode.Episode)}",
+                        firstEpisode.Summary);
+                }))
+            .SelectMany(x => x));
+
+    private static DiscordEmbedMessage WriteMovieToDiscord(this IEnumerable<IMedia> medias) =>
+        new("**\ud83c\udfa5 Films :**",medias
+        .Where(m => m.Type == MediaType.Movie)
+        .Select(m => new DiscordEmbedField($"**{m.Title}**", m.Summary)));
 }
