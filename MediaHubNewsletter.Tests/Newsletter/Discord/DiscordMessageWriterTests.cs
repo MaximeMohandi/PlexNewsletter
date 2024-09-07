29dc348ad8f010053c6346706b7a71dc57a0279a
Maxime Mohandi <mohandi.maxime@gmail.com>
2024-09-07 22:16:40 +0200

    feat: 🎸 only display first and last added episode by season

--- MediaHubNewsletter.Tests/Newsletter/Discord/DiscordMessageWriterTests.cs
+++ MediaHubNewsletter.Tests/Newsletter/Discord/DiscordMessageWriterTests.cs
@@ -70,4 +70,50 @@ public class DiscordMessageWriterTests
 
         result.Should().BeEquivalentTo(expected);
     }
+
+    [Test]
+    public void ShouldAggregateWhenEpisodeFollows()
+    {
+       var expected = new DiscordMessage(new[]
+       {
+           new DiscordEmbedMessage("**\ud83d\udcfa Séries :**", new[]
+           {
+               new DiscordEmbedField("**The Mandalorian** - S01E02-04", "A Mandalorian bounty hunter tracks a target for a well-paying client."),
+           }),
+           new DiscordEmbedMessage("**\ud83c\udfa5 Films :**", Enumerable.Empty<DiscordEmbedField>())
+       });
+
+       var result = DiscordMessageWriter.Write(new[]
+       {
+           new FakeMedia()
+           {
+               Title = "Chapter 1",
+               Season = 1,
+               Episode = 2,
+               Type = MediaType.TvShow,
+               Summary = "A Mandalorian bounty hunter tracks a target for a well-paying client.",
+               TvShow = "The Mandalorian"
+           },
+           new FakeMedia()
+           {
+               Title = "Chapter 2",
+               Season = 1,
+               Episode = 3,
+               Type = MediaType.TvShow,
+               Summary = "The battered Mandalorian returns to his client for his reward.",
+               TvShow = "The Mandalorian"
+           },
+           new FakeMedia()
+           {
+               Title = "Chapter 3",
+               Season = 1,
+               Episode = 4,
+               Type = MediaType.TvShow,
+               Summary = "The Mandalorian teams up with an ex-soldier to protect a village from raiders.",
+               TvShow = "The Mandalorian"
+           }
+       });
+
+       result.Should().BeEquivalentTo(expected);
+    }
 }
--- MediaHubNewsletter/Newsletter/Discord/DiscordMessageWriter.cs
+++ MediaHubNewsletter/Newsletter/Discord/DiscordMessageWriter.cs
@@ -11,15 +11,36 @@ public static class DiscordMessageWriter
             .Select(m => new DiscordEmbedField($"**{m.Title}**", m.Summary));
 
         var tvShowFields = media
-            .Where(m => m.Type == MediaType.TvShow)
-            .Select(m => new DiscordEmbedField($"**{m.TvShow}** - S{FormatToSxxEyy(m.Season)}E{FormatToSxxEyy(m.Episode)}", m.Summary));
+            .Where(m => m.Type == MediaType.TvShow);
+
 
         return new DiscordMessage(new List<DiscordEmbedMessage>
         {
             new("**\ud83c\udfa5 Films :**", movieFields),
-            new("**\ud83d\udcfa Séries :**", tvShowFields)
+            new("**\ud83d\udcfa Séries :**", AggregateFollowingEpisode(tvShowFields))
         });
     }
 
     private static string FormatToSxxEyy(int number) => number <= 9 ? $"0{number}" : number.ToString();
+
+    private static IEnumerable<DiscordEmbedField> AggregateFollowingEpisode(IEnumerable<IMedia> medias)
+    {
+        var groupedTvShows = medias
+            .GroupBy(m => m.TvShow)
+            .Select(shows => shows
+                .GroupBy(m => m.Season)
+                .Select(season =>
+                {
+                   if(season.Count() > 1)
+                   {
+                       var firstEpisode = season.First();
+                       var lastEpisode = season.Last();
+                       return new DiscordEmbedField($"**{firstEpisode.TvShow}** - S{FormatToSxxEyy(firstEpisode.Season)}E{FormatToSxxEyy(firstEpisode.Episode)}-{FormatToSxxEyy(lastEpisode.Episode)}", firstEpisode.Summary);
+                   }
+                       var episode = season.First();
+                       return new DiscordEmbedField($"**{episode.TvShow}** - S{FormatToSxxEyy(episode.Season)}E{FormatToSxxEyy(episode.Episode)}", episode.Summary);
+                }));
+
+        return groupedTvShows.SelectMany(x => x);
+    }
 }
