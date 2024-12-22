deb78046df19eec5886883543ea2734fa6d119ce
Maxime Mohandi <mohandi.maxime@gmail.com>
2024-12-22 10:42:23 +0100

    fix: 🐛 format the json so that properties are camelcase

--- MediaHubNewsletter/Newsletter/Discord/DiscordDistribution.cs
+++ MediaHubNewsletter/Newsletter/Discord/DiscordDistribution.cs
@@ -7,22 +7,26 @@ namespace MediahubNewsletter.Newsletter.Discord;
 
 public class DiscordDistribution : IDistributionCanal
 {
-   private readonly IMediaLibrary _mediaLibrary;
-   private readonly string? _discordWebhook;
-   private readonly HttpClient _client;
+    private readonly HttpClient _client;
+    private readonly string? _discordWebhook;
+    private readonly IMediaLibrary _mediaLibrary;
 
-   public DiscordDistribution(IMediaLibrary mediaLibrary, HttpClient client, IConfiguration configuration)
-   {
+    public DiscordDistribution(IMediaLibrary mediaLibrary, HttpClient client, IConfiguration configuration)
+    {
         _client = client;
         _mediaLibrary = mediaLibrary;
         _discordWebhook = configuration["DiscordClient:webhookUrl"];
-   }
+    }
 
-   public async Task Send()
-   {
-       var message = DiscordMessageWriter.Write(await _mediaLibrary.RecentlyAddedMedia());
-       var json = JsonSerializer.Serialize(message);
-       var content = new StringContent(json, Encoding.UTF8, "application/json");
-       await _client.PostAsync(_discordWebhook, content);
-   }
+    public async Task Send()
+    {
+        var message = DiscordMessageWriter.Write(await _mediaLibrary.RecentlyAddedMedia());
+        var jsonConfiguration = new JsonSerializerOptions
+        {
+            PropertyNamingPolicy = JsonNamingPolicy.CamelCase, WriteIndented = true
+        };
+        var json = JsonSerializer.Serialize(message, jsonConfiguration);
+        var content = new StringContent(json, Encoding.UTF8, "application/json");
+        await _client.PostAsync(_discordWebhook, content);
+    }
 }
